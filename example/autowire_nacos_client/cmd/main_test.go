a7c57d43410af60d08fa7d3396b3bcb75f8c72fc
LaurenceLiZhixin <382673304@qq.com>
2022-05-23 22:57:42 +0800

    fix: add nacos retries and ut license header

--- example/autowire_nacos_client/cmd/main_test.go
+++ example/autowire_nacos_client/cmd/main_test.go
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022, Alibaba Group;
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
@@ -32,6 +47,16 @@ func testGetAndSetService(t *testing.T, client normalNacos.NacosClient, serviceN
 		Port:        1999,
 		ServiceName: serviceName,
 	})
+	retries := 0
+	for err != nil && retries < 3 {
+		time.Sleep(time.Second * 10)
+		_, err = client.RegisterInstance(vo.RegisterInstanceParam{
+			Ip:          "127.0.0.1",
+			Port:        1999,
+			ServiceName: serviceName,
+		})
+		retries++
+	}
 	if err != nil {
 		panic(err)
 	}
@@ -54,14 +79,7 @@ func TestNacosClient(t *testing.T) {
 		return
 	}
 	assert.Nil(t, docker_compose.DockerComposeUp("../docker-compose/docker-compose.yaml", time.Second*10))
-	err := ioc.Load()
-	retries := 0
-	for err != nil && retries < 3 {
-		time.Sleep(time.Second * 10)
-		err = ioc.Load()
-		retries++
-	}
-	assert.Nil(t, err)
+	assert.Nil(t, ioc.Load())
 	appInterface, err := singleton.GetImpl("App-App")
 	assert.Nil(t, err)
 	app := appInterface.(*App)
