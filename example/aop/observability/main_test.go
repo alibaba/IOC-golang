5e61f624c074514a8ac04b371f936e7199e2c746
LaurenceLiZhixin <382673304@qq.com>
2022-07-26 14:05:17 +0800

    Fix: add missing license, fix parallel test problem

--- example/aop/observability/main_test.go
+++ example/aop/observability/main_test.go
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022, Alibaba Group;
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
@@ -31,17 +46,8 @@ github.com/alibaba/ioc-golang/example/aop/observability.ServiceImpl2
 [GetHelloString]
 
 `, output)
-}
 
-func TestObservabilityMonitor(t *testing.T) {
-	assert.Nil(t, ioc.Load())
-	app, err := GetAppIOCInterfaceSingleton()
-	assert.Nil(t, err)
-	go func() {
-		app.Run()
-	}()
-	time.Sleep(time.Millisecond * 500)
-	output, err := iocli_command.Run([]string{"monitor", "-i", "3"}, time.Second*4)
+	output, err = iocli_command.Run([]string{"monitor", "-i", "3"}, time.Second*4)
 	assert.Nil(t, err)
 	assert.True(t, strings.Contains(output, `github.com/alibaba/ioc-golang/example/aop/observability.ServiceImpl1.GetHelloString()
 Total: 1, Success: 1, Fail: 0, AvgRT: `))
